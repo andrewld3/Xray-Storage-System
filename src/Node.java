e348b9ba8b640daceca18c06c442340784fdab90
andrew <adriscol@oswego.edu>
2018-10-10 20:23:03 -0400

    Node Class Written

--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,32 @@
 public class Node {
     //TODO: Write Node Structure
+    int data;
+    Node left;
+    Node right;
+
     //TODO: Write Node Operations
+
+    public void setData(int d) {
+        data = d;
+    }
+
+    public int getData( ) {
+        return data;
+    }
+
+    public void setLeft(Node n) {
+        left = n;
+    }
+
+    public Node getLeft( ) {
+        return left;
+    }
+
+    public void setRight(Node n) {
+        right = n;
+    }
+
+    public Node getRight( ) {
+        return right;
+    }
 }

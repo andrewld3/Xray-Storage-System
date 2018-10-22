61788411cb5488b745472ff82e270b905f85b16b
Andrew Driscoll <adriscol@oswego.edu>
2018-10-21 21:37:27 -0400

    WIP- Code Storage WIP- Filename Parsing Complete- Split Function

--- src/Data.java
+++ src/Data.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class Data {
+
+    int[] codes;
+    int numFiles;
+
+    public Data(int n) {
+        codes = new int[n];
+    }
+
+    public void addCode(int i, int c) {
+        codes[i] = c;
+        numFiles++;
+    }
+
+    public void displayCode( ) {
+        for(int i = 0; i < numFiles; i++) {
+            System.out.print()
+        }
+    }
 }
